081603a0abe0b0459ea3b5c9e7f9069ab2e54be4
kissagnesa <sindzse88@gmail.com>
2023-12-22 14:27:16 +0100

    Javascript is created.

---
+++ dance.js
@@ -0,0 +1,8 @@
+function elkuldes() {
+    var nev = document.getElementById('nev').value;
+    var kor = document.getElementById('kor').value;
+    var muveszetiMufaj = document.getElementById('muveszetiMufaj').value;
+    var telefonszam = document.getElementById('telefonszam').value;
+    var email = document.getElementById('email').value;
+
+}
