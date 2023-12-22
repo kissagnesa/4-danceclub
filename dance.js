411515669cb4a8e03b9b1637b3f9dfe00f4c7c0b
kissagnesa <sindzse88@gmail.com>
2023-12-22 15:30:06 +0100

    HTML, CSS and JS are done.

--- dance.js
+++ dance.js
@@ -1,8 +1,10 @@
 function elkuldes() {
     var nev = document.getElementById('nev').value;
     var kor = document.getElementById('kor').value;
-    var muveszetiMufaj = document.getElementById('muveszetiMufaj').value;
+    var tancnem = document.getElementById('tancnem').value;
     var telefonszam = document.getElementById('telefonszam').value;
     var email = document.getElementById('email').value;
+
+    alert("Sikeres jelentkezés!")
 
 }
